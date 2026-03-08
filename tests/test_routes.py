616611cd6cf545b63d92771ee0d604c1ed00e7ab
Aviral3501 <aviralsingh3501@gmail.com>
2026-03-08 00:37:40 -0500

    added tests and improved coverage to 98%

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,6 +7,8 @@ Test cases can be run with the following:
 """
 import os
 import logging
+import importlib
+from unittest.mock import patch
 from unittest import TestCase
 from tests.factories import AccountFactory
 from service.common import status  # HTTP Status Codes
@@ -134,4 +136,57 @@ class TestAccountService(TestCase):
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_account_list(self):
+        """It should Get a list of Accounts"""
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
+
+    def test_update_account(self):
+        """It should Update an existing Account"""
+        # create an Account to update
+        test_account = AccountFactory()
+        resp = self.client.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # update the account
+        new_account = resp.get_json()
+        new_account["name"] = "Something Known"
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account["name"], "Something Known")
+
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_init_db_failure_exits(self):
+        """Test that the service exits if init_db fails"""
+        with patch("service.models.init_db") as mock_init_db:
+            mock_init_db.side_effect = Exception("DB failure")
+
+            with patch("sys.exit") as mock_exit:
+                import service
+                importlib.reload(service)
+
+                mock_exit.assert_called_with(4)
+
+    def test_update_account_not_found(self):
+        """It should not Update an Account that is not found"""
+        account = AccountFactory().serialize()
+
+        resp = self.client.put(f"{BASE_URL}/0", json=account)
+
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
